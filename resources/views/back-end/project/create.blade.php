08e5234062a941197c80c1b55c387a866bf2c667
gittahutaridewinzha <gittahutari81@gmail.com>
2025-05-08 16:04:39 +0700

    push
    push

--- resources/views/back-end/project/create.blade.php
+++ resources/views/back-end/project/create.blade.php
@@ -32,4 +32,10 @@
         </div>
     </div>
 </div>
+@section('scripts')
+<script src="https://cdn.ckeditor.com/4.20.2/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace('deskripsi');
+</script>
+@endsection
 @endsection
