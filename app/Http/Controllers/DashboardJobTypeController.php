adb6310d018902194796c2e5294952985385064d
hurulainytsaltsa <hurulainy01@gmail.com>
2025-05-08 14:07:03 +0700

    Job Type

--- app/Http/Controllers/DashboardJobTypeController.php
+++ app/Http/Controllers/DashboardJobTypeController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobType;
 use Illuminate\Http\Request;
 
 class DashboardJobTypeController extends Controller
 {
-    //
+    public function index()
+    {
+        $jobType = JobType::all();
+        return view('back-end.pages.job_types.index', compact('jobType'));
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name'       => 'required|string|max:255',
+        ]);
+
+        $jobType = JobType::create($validatedData);
+
+        return redirect()->back()->with('success', 'Berhasil disimpan!');
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $jobType = JobType::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'name'     => 'required|string|max:255',
+        ]);
+
+        $jobType->name = $request->name;
+        $jobType->save();
+
+        return redirect()->back()->with('success', 'Berhasil Diperbarui');
+    }
+
+    public function destroy(string $id)
+    {
+        $jobType = JobType::findOrFail($id);
+
+        $jobType->delete();
+
+        return redirect()->back()->with('success', 'Berhasil dihapus!');
+    }
 }
