e638be779fbc20585f1e44d3d07f84940cda2ae1
hurulainytsaltsa <hurulainy01@gmail.com>
2025-05-08 13:50:07 +0700

    Push lu wan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardDetailFiturController;
+use App\Http\Controllers\DashboardFiturController;
+use App\Http\Controllers\DashboardJobTypeController;
+use App\Http\Controllers\DashboardPengerjaanController;
+use App\Http\Controllers\DashboardPenugasanController;
+use App\Http\Controllers\DashboardProjectController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\RolesController;
 use App\Http\Middleware\CheckMenuAccess;
@@ -9,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['auth:admin', CheckMenuAccess::class])->group(function () {
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource('/roles', RolesController::class );
-Route::resource('/register', RegisterController::class);
+    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/roles', RolesController::class);
+    Route::resource('/register', RegisterController::class);
+    Route::resource('/job-type', DashboardJobTypeController::class);
+    Route::resource('/project', DashboardProjectController::class);
+    Route::resource('/fitur', DashboardFiturController::class);
+    Route::resource('/detail-fitur', DashboardDetailFiturController::class);
+    Route::resource('/pengerjaan', DashboardPengerjaanController::class);
+    Route::resource('/penugasan', DashboardPenugasanController::class);
 });
 
 Route::get('/login', [AuthController::class, 'index'])->name('login');
