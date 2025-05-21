b594f30f4c2426e0231f6da1c280c28449932534
gittahutaridewinzha <gittahutari81@gmail.com>
2025-05-21 16:53:14 +0700

    login
    login

--- resources/views/back-end/layouts/main.blade.php
+++ resources/views/back-end/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Sunset Hill Admin</title>
+    <title>Timeline Project</title>
     <!-- Tambahkan ini di bagian <head> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
--- resources/views/back-end/pages/otoritas/login.blade.php
+++ resources/views/back-end/pages/otoritas/login.blade.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" />
 </head>
+
 <body class="bg-[#f9f9f9]">
 
-<div class="min-h-screen flex items-center justify-center">
-    <div class="flex w-full max-w-screen-xl shadow-lg rounded-lg overflow-hidden">
+    <div class="min-h-screen flex items-center justify-center">
 
-        <!-- Kiri: Gambar -->
-        <div class="w-1/2 bg-cover bg-center relative hidden md:block" style="background-image: url('assets/images/background/contact.jpg');">
 
-        </div>
+
+
+
 
         <!-- Kanan: Form -->
-        <div class="w-full md:w-1/2 bg-white px-8 py-12 flex flex-col justify-center">
+        <div class="w-[28rem] bg-white p-8 rounded shadow justify-center">
+
             <div class="flex justify-center mb-6">
-                <img src="{{ asset('assets/images/konfigurasi/logo.png') }}" alt="Logo" class="h-12 w-auto">
+
+                <img src="{{ asset(path: 'images/favicon.png') }}" alt="Logo" class="h-12 w-auto">
             </div>
             <h2 class="text-2xl font-semibold mb-4 text-gray-800">Selamat datang kembali!</h2>
 
@@ -36,13 +40,15 @@
                 <!-- Email -->
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700 mb-1">Email</label>
-                    <input type="email" name="email" class="w-full border border-gray-300 rounded px-3 py-2" required>
+                    <input type="email" name="email" class="w-full border border-gray-300 rounded px-3 py-2"
+                        required>
                 </div>
 
                 <!-- Password -->
                 <div class="mb-4">
                     <label class="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                    <input type="password" name="password" class="w-full border border-gray-300 rounded px-3 py-2" required>
+                    <input type="password" name="password" class="w-full border border-gray-300 rounded px-3 py-2"
+                        required>
                 </div>
 
                 <!-- Tombol Login -->
@@ -51,8 +57,9 @@
                 </button>
             </form>
         </div>
+
     </div>
-</div>
 
 </body>
+
 </html>
